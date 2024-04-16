27db526619728657befad9b2034a1e38654b9c2a
shuchendra <124861829+shuchendra@users.noreply.github.com>
2024-04-16 12:23:08 +0530

    Add files via upload

---
+++ build/precache-manifest.b1f30f4e0b475a0a4bbe9803004c272f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d0f5e44a1fe5483f873b32db7a7e494",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4effed767558745203d",
+    "url": "/static/css/main.25278eba.chunk.css"
+  },
+  {
+    "revision": "915bfc459e04384b13d3",
+    "url": "/static/js/2.0969c805.chunk.js"
+  },
+  {
+    "revision": "d9b97b3a1e15e18d1669b641b59f14cf",
+    "url": "/static/js/2.0969c805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57fc7c62a5ce6883a095",
+    "url": "/static/js/3.514aa520.chunk.js"
+  },
+  {
+    "revision": "b4effed767558745203d",
+    "url": "/static/js/main.0b281b0f.chunk.js"
+  },
+  {
+    "revision": "8bd162b85b81e991e803",
+    "url": "/static/js/runtime-main.f3d3634c.js"
+  }
+]);
